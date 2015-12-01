94edd044d01530712821a4210ac9f4e1c9937b12
Chris Forno <jekor@jekor.com>
2015-12-01 12:13:50 +0800

    This wasn't doing what I thought it was.

--- jen-vbox.nix
+++ jen-vbox.nix
@@ -3,11 +3,5 @@
     deployment.targetEnv = "virtualbox";
     deployment.virtualbox.memorySize = 2048;
     deployment.virtualbox.headless = true;
-    deployment.virtualbox.disks = {
-      boot = {
-        port = 1;
-        size = 20971520;
-      };
-    };
   };
 }
