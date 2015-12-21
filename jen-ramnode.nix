1259d53e85549c62ff0c5b1d11c93a8bb922d3ed
Chris Forno <jekor@jekor.com>
2015-12-21 16:55:10 +0800

    Creating daily database backups for Vocabulink.

--- jen-ramnode.nix
+++ jen-ramnode.nix
@@ -1,5 +1,9 @@
 {
-  www = {
+  www = {config, pkgs, ...}:
+  let
+    gpg = pkgs.gnupg1orig;
+  in
+  {
     deployment.targetEnv = "none";
     deployment.targetHost = "168.235.79.181";
 
@@ -11,5 +15,10 @@
       device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
+
+    services.cron.enable = true;
+    services.cron.systemCronJobs = [
+      "0 0 * * * root ${pkgs.postgresql}/bin/pg_dump -U vocabulink --create vocabulink | ${gpg}/bin/gpg --keyring ${./security/backups.gpg} -e -r 'jekor@jekor.com' --trust-model always | ${pkgs.s3cmd}/bin/s3cmd --access_key ${builtins.readFile ./security/vocabulink-backup-aws-access-key} --secret_key ${builtins.readFile ./security/vocabulink-backup-aws-secret-key} put - s3://vocabulink.com-archive/sql/vocabulink--$(date --iso-8601).sql.gpg"
+    ];
   };
 }
